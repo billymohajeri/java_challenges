d70f687a9c0ee29459adaac6e0cd44b6948d1834
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-10 16:11:43 +0300

    Add prompt for second number

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Enter two numbers:");
-    System.out.print("First numbers: ");
+    System.out.print("1st numbers: ");
     Scanner scanner = new Scanner(System.in);
     double firstNumber = Double.parseDouble(scanner.nextLine());
+    System.out.print("2nd numbers: ");
+    double secondNumber = Double.parseDouble(scanner.nextLine());
+    System.out.println(firstNumber + secondNumber);
   }
 }
