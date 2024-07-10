d7887e847e97a23e030dd456f654780b2398a007
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-10 16:02:56 +0300

    Add prompt for first number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Enter two numbers:");
+    System.out.print("First numbers: ");
+    Scanner scanner = new Scanner(System.in);
+    double firstNumber = Double.parseDouble(scanner.nextLine());
+  }
 }
