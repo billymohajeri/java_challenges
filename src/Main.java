356bc7d24e6e9d65f583d4630384bedcc69e0a65
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-11 00:55:06 +0300

    Add the menu

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,22 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Enter two numbers:");
+    final String YELLOW = "\033[0;33m";
+
+    System.out.println(YELLOW + "Enter two numbers:");
+
     System.out.print("1st numbers: ");
     Scanner scanner = new Scanner(System.in);
     double firstNumber = Double.parseDouble(scanner.nextLine());
+
     System.out.print("2nd numbers: ");
     double secondNumber = Double.parseDouble(scanner.nextLine());
-    System.out.println(firstNumber + secondNumber);
+
+    System.out.printf("%nChoose an operation (Enter 1 to 4):%n");
+    System.out.printf("1. Addition (+)%n2. Subtraction (-)%n3. Multiplication (*)%n4. division (÷)%n");
+    String operation = scanner.nextLine();
+//    System.out.println(firstNumber + secondNumber);
   }
+
+
 }
