fd4c7f640bb583aeb8e020297295aab643a914fc
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-11 12:27:32 +0300

    Add book class

--- src/OOP.java
+++ src/OOP.java
@@ -1,2 +1,24 @@
+import java.util.Date;
+
 public class OOP {
+  public static void main(String[] args) {
+  }
 }
+
+class Book {
+  private String title;
+  private String ISBN;
+  private Author[] authors;
+  private Date borrowedAt;
+
+  Book(String title, String ISBN, Author[] authors, Date borrowedAt) {
+    this.title = title;
+    this.ISBN = ISBN;
+    this.authors = authors;
+    this.borrowedAt = borrowedAt;
+  }
+}
+
+
+}
+
