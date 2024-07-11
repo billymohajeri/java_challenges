fd26bc15f454ec455367aea7cbc7e4123355b63b
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-11 11:16:06 +0300

    Add the main loop

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class Game {
   public static void main(String[] args) {
-    System.out.println("I have chosen a number. Try to guess it:");
+    final String RED = "\033[0;31m";
+    final String BLUE = "\033[0;34m";
+
+    int counter = 0;
+    int userNumber = 0;
+    int randomNumber = (int) (Math.random() * 100) + 1;
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(BLUE + "I have chosen an integer between 1 and 100.");
+    System.out.print("Try to guess it: ");
+    do {
+      try {
+        System.out.println("try again");
+        userNumber = Integer.parseInt(scanner.nextLine());
+        counter++;
+      } catch (Exception e) {
+        System.out.println(RED + "Invalid input. " + e.getMessage());
+        continue;
+      }
+
+      if (userNumber > randomNumber) {
+        System.out.println("Your number is too high. Try a lower number!");
+      } else if (userNumber < randomNumber) {
+        System.out.println("Your guess is too low. Try a higher number!");
+      } else {
+        System.out.printf("Congratulations!%nYou've guessed the number correctly with %d guess(es).%n", counter);
+      }
+    } while (randomNumber != userNumber);
+
   }
 
 }
